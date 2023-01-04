135cd595c61475ba6206b7985fb0c3a4497712ff
Svetoslav Krasimirov Petkov <98747798+PetkovSvetoslav@users.noreply.github.com>
2023-01-04 14:03:53 +0200

    Delete EXAMS/10-Apr-2021 directory

--- EXAMS/10-Apr-2021/12-Dec-2020/Solutions/BankSafe/src/bankSafe/Main.java
+++
@@ -1,7 +0,0 @@
-package bankSafe;
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
--- EXAMS/10-Apr-2021/12-Dec-2020/Solutions/bakery/entities/bakedFoods/Bread.java
+++
@@ -1,9 +0,0 @@
-package bakery.entities.bakedFoods;
-
-public class Bread extends BaseFood {
-    private static final int INITIAL_BREAD_PORTION = 200;
-
-    public Bread(String name, double price) {
-        super(name, INITIAL_BREAD_PORTION, price);
-    }
-}
