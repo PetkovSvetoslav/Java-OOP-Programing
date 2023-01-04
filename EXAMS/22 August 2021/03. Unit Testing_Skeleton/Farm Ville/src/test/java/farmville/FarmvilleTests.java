2e6ae98621c6df9c3af0be88aac2464284ab5bf3
Svetoslav Krasimirov Petkov <98747798+PetkovSvetoslav@users.noreply.github.com>
2023-01-04 14:18:04 +0200

    Add files via upload

---
+++ EXAMS/22 August 2021/03. Unit Testing_Skeleton/Farm Ville/src/test/java/farmville/FarmvilleTests.java
@@ -0,0 +1,14 @@
+package farmville;
+
+import org.junit.Test;
+
+public class FarmvilleTests {
+    //TODO: TEST ALL THE FUNCTIONALITY OF THE PROVIDED CLASS Farm
+    @Test
+    public void test(){
+
+    }
+
+
+
+}
